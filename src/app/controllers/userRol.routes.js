cf6842c818b1e9f54a63a23a56739ea64f6f6ef5
jgaleano2018 <32717587+jgaleano2018@users.noreply.github.com>
2025-03-08 16:43:15 -0500

    Se adiciono cambios de la rais del proyecto.

--- src/app/controllers/userRol.routes.js
+++ src/app/controllers/userRol.routes.js
@@ -9,6 +9,6 @@ const router = express.Router();
 
 router.post("/userRol", postUserRol);
 router.get("/userRol", getUserRol);
-router.get("/userRol:id", getUserRolById);
+router.get("/userRol/:id", getUserRolById);
 
 export default router;
